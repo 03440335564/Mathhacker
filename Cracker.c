efbb18a631f353188995f92724114b2a0c6fb206
03440335564 <63583374+03440335564@users.noreply.github.com>
2020-04-14 09:23:53 +0500

    New version cracker.c

--- Cracker.c
+++ Cracker.c
@@ -1,52 +1,52 @@
-#include<stdio.h>
-#include<math.h> //used for pow() and sqrt() function
-
-int main() 
-{
-	
-	
-	char operation;
-	float toatal, num1,num2,result;
-	printf("\t\t\tHACKER ISRAR CALCULATOR\n");
-	printf("\t\t\t=======================\n");
-	printf("\nenter 1st number: ");
-	scanf("%f",&num1);
-	printf("\n\nentter the operation: ");
-	operation= getchar();
-	printf("\nenter 2nd number: ");
-	scanf("%f",&num2);
-	switch (operation)
-	{
-	
-	case'+':
-	result=num1+num2;
-	printf("\nresult = %3f",result);
-	break;
-	case'-':
-	result=num1-num2;
-	printf("result=%3f",result);
-	break;
-	case'*':
-	result=num1*num2;
-	printf("result=%3f",result);
-	break;
-	case'/':
-	result=num1/num2;
-	printf("result=%3f",result);
-	break;
-		case'%':
-			result=(num1*100)/toatal;
-			printf("result = %3f",result);
-			
-			break;
-			
-
-	
-	default:
-		printf("\ninvalid operation"); 
-}
-	
-	getchar();
-}
-
+/*C program to design calculator with basic operations using switch.*/
  
+#include <stdio.h>
+ 
+int main()
+{
+    int num1,num2;
+    float result;
+    char ch;    //to store operator choice
+printf("\tWELCOME TO MY CALACULATOR") ;
+printf("\t===============================\n"); 
+printf("use + for addition \n use -  for subtraction \n use / for divide \n use * for multiply\n\n") ;
+
+     printf("==========================================\n\n");
+    printf("Enter first number: ");
+    scanf("%d",&num1);
+printf("Choose operation to perform:  ");
+    scanf(" %c",&ch);
+    printf("Enter second number: ");
+    scanf("%d",&num2);
+     
+    
+     
+    result=0;
+    switch(ch)    
+    {
+        case '+':
+            result=num1+num2;
+            break;
+             
+        case '-':
+            result=num1-num2;
+            break;
+         
+        case '*':
+            result=num1*num2;
+            break;
+             
+        case '/':
+            result=(float)num1/(float)num2;
+            break;
+             
+        case '%':
+            result=num1%num2;
+            break;
+default:
+            printf("Invalid operation.\n");
+    }
+ 
+    printf("Result: %d %c %d = %f\n",num1,ch,num2,result);
+    return 0;
+}
